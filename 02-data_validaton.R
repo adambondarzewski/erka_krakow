4f9f67e8bee742c3d56c3c3ef7c657c14ed86112
patyk11 <patyk11@gmail.com>
2016-07-17 14:43:37 +0200

    Data validation

--- 02-data_validaton.R
+++ 02-data_validaton.R
@@ -1,14 +1,12 @@
 library(data.table)
-head(DT)
-summary(DT)
 
 ######################################3
-# rowing out the observations with less then half of answers, not to bias cross validation
-DT<-DT[QuestionsCompleted< 16]
+# rowing out the observations with less then half of answers, in order not to bias cross validation
+DT<-DT[QuestionsCompleted> 16]
 ####################################33
 #
 #
-table(DT$Score)
+
 comparison<-table(DT$Score, DT$Gender)
 ############################3333
 # Comparing Score Distributions
@@ -23,8 +21,32 @@ sm.density.compare(DT$Score/DT$QuestionsCompleted, cyl.f, xlab="Score")
 title(main="Dstribution of Scores by gender")
 
 Sa_cols <- c(paste0('An0', 1:9), paste0('An', 10:32))
-# some testing
-DT[apply(DT[, Sa_cols, with = FALSE], 1, mean, na.rm = TRUE) ==1 ]
-DT[apply(DT[, Sa_cols, with = FALSE], 1, mean, na.rm = TRUE) ==0 ]
+Sc_cols <- c(paste0('Sc0', 1:9), paste0('Sc', 10:32))
+Sd_cols <- c(paste0('Du0', 1:9), paste0('Du', 10:32))
 
+
+#  only Same or Not the Same , Same= 1 not the same =0
+#
+#
+if (sum(apply(DT[, Sa_cols, with = FALSE]=="Same", 1, mean, na.rm = TRUE) %in% c(0,1))  >0){
+    IND1<-which(apply(DT[, Sa_cols, with = FALSE]=="Same", 1, mean, na.rm = TRUE) %in% c(0,1))
+    DT<-DT[!IND1,]
+}
+
+# checking duration
+#
+#
+#calculating average durations
+mean_duration_qestion<-apply(DT[, Sd_cols, with = FALSE], 2, mean, na.rm = TRUE) 
+DT$mean_duration_person<-apply(DT[, Sd_cols, with = FALSE], 1, mean, na.rm = TRUE) 
+
+
+boxplot(DT$mean_duration_person)
+#acceptable_duration - a feasible number for actually performing the task.
+acceptable_duration=2
+#throwing out the duration
+if(sum(DT$mean_duration_person<acceptable_duration)>0)
+  DT<-DT[mean_duration_person>acceptable_duration,]
+#
+#
 
