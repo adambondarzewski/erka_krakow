88052db1383af30fe1abaec0f2e4fc365d74901d
adambondarzewski <adambondarzewski@gmail.com>
2016-07-23 17:48:24 +0200

    correct_score_fun srcipt added - fun for optimisation

---
+++ 07b-correct_score_fun.R
@@ -0,0 +1,52 @@
+Sc_cols <- c(paste0('Sc0', 1:9), paste0('Sc', 10:32))
+Sd_corrected_cols <- c(paste0('corrected_Du0', 1:9), paste0('corrected_Du', 10:32))
+
+DT_shrinked <- DT[, c(Sc_cols, Sd_corrected_cols), with = FALSE]
+
+# produce 
+
+score <- function(row, w, v, lambda, penalty = function(x) x^2) {
+  
+  length_answers <- length(na.omit(row[Sc_cols]))
+  
+  output <- w * row[Sc_cols] + v * row[Sd_corrected_cols] + lambda * penalty(w - 1/32)
+  
+  output <- output/length_answers
+  
+  return(sum(output, na.rm = TRUE))
+}
+
+calculate_score <- function(DT, w, v, lambda, score_fun = score) {
+    
+    DT$score_measured <- apply(DT_shrinked, 1, function(x) {score_fun(x, w = w, v = v, lambda)})
+    
+    DT_groups <- DT[, .(score_measured = mean(score_measured), group_count = .N)
+                    , by =.(Gender, SubGroup1)]
+    
+    DT_groups_counts <- DT_groups[, .(group_count = sum(group_count)), by = SubGroup1]
+    
+    DT_groups_casted <- dcast(DT_groups[ , .(SubGroup1, Gender, score_measured)], SubGroup1 ~ Gender)
+    
+    DT_groups_casted <- merge(DT_groups_casted, DT_groups_counts
+                              , by ='SubGroup1')
+    
+    participants_sum <- DT_groups_casted[, sum(group_count)]
+    DT_groups_casted[, group_part := group_count/participants_sum]
+    DT_groups_casted[, gender_gap := (`F` - `M`)^2 * participants_sum]
+    
+    # todo: ważyć ilością
+  
+  return(DT_groups_casted[, sum(gender_gap, na.rm = TRUE)])
+}
+
+calculate_score_wrapper <- function(pars) {
+  
+  w <- pars[1:32]
+  v <- pars[33:64]
+    
+  calculate_score(DT, w = w, v = v, lambda = 1)
+}
+
+answer <- optim(par = rep(1/32, 64), calculate_score_wrapper
+                , lower = rep(0, 64)
+                , upper = rep(1, 64))
